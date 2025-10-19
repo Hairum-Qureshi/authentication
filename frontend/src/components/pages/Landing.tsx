a765df676a24b29533c656232126e0b4e0787a0a
Hairum Qureshi <hqureshi@udel.edu>
2025-10-18 21:14:37 -0400

    started designing landing page and styled left side

--- frontend/src/components/pages/Landing.tsx
+++ frontend/src/components/pages/Landing.tsx
@@ -1,3 +1,13 @@
 export default function Landing() {
-	return <div>Home</div>;
+	return (
+		<div className="w-full min-h-screen max-h-auto bg-gradient-to-r from-slate-800 to-slate-950 text-white">
+			<div className="flex lg:flex-row flex-col w-full min-h-screen max-h-auto">
+				<div className="lg:w-1/2 flex flex-col items-center justify-center lg:mt-0 mt-20">
+					<h1 className="text-8xl font-bold">Aura Auth</h1>
+					<h3 className="text-2xl mt-1 text-slate-50">Purely Authentication</h3>
+				</div>
+				<div className="lg:w-1/2"></div>
+			</div>
+		</div>
+	);
 }
