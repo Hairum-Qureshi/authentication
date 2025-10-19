b0ff69b8b554f73b7f9860c16ff50c08015b1473
Hairum Qureshi <hqureshi@udel.edu>
2025-10-18 23:45:47 -0400

    darkened background gradient

--- frontend/src/components/pages/Landing.tsx
+++ frontend/src/components/pages/Landing.tsx
@@ -1,6 +1,6 @@
 export default function Landing() {
 	return (
-		<div className="w-full min-h-screen max-h-auto bg-gradient-to-r from-slate-800 to-slate-950 text-white">
+		<div className="w-full min-h-screen max-h-auto bg-gradient-to-r from-slate-900 to-slate-950 text-white">
 			<div className="flex lg:flex-row flex-col w-full min-h-screen max-h-auto">
 				<div className="lg:w-1/2 flex flex-col items-center justify-center lg:mt-0 mt-20">
 					<h1 className="text-8xl font-bold">Aura Auth</h1>
