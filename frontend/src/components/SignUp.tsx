8d8ec35028902e32124bf2bcd6bb1893f18d49ce
Hairum Qureshi <hqureshi@udel.edu>
2025-10-19 21:02:44 -0400

    added focus outlines to input and hover effect to buttons

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -5,36 +5,35 @@ export default function SignUp() {
 				<input
 					type="text"
 					placeholder="First Name"
-					className="rounded-md p-3 w-1/2 bg-slate-800 outline-none"
+					className="rounded-md p-3 w-1/2 bg-slate-800 outline-none focus:outline-none focus:border-sky-500 focus:border"
 				/>
 				<input
 					type="text"
 					placeholder="Last Name"
-					className="rounded-md p-3 w-1/2 bg-slate-800 outline-none"
+					className="rounded-md p-3 w-1/2 bg-slate-800 outline-none focus:outline-none focus:border-sky-500 focus:border"
 				/>
 			</div>
 			<input
 				type="email"
 				placeholder="Email"
-				className="rounded-md p-3 bg-slate-800 outline-none"
+				className="rounded-md p-3 bg-slate-800 outline-none focus:outline-none focus:border-sky-500 focus:border"
 			/>
 			<input
 				type="password"
 				placeholder="Password"
-				className="rounded-md p-3 bg-slate-800 outline-none"
+				className="rounded-md p-3 bg-slate-800 outline-none focus:outline-none focus:border-sky-500 focus:border"
 			/>
 			<input
 				type="password"
 				placeholder="Confirm Password"
-				className="rounded-md p-3 bg-slate-800 outline-none"
+				className="rounded-md p-3 bg-slate-800 outline-none focus:outline-none focus:border-sky-500 focus:border"
 			/>
 			<button
 				type="submit"
-				className="flex w-full p-2 justify-center rounded-md bg-sky-600"
+				className="flex w-full p-2 justify-center rounded-md bg-sky-600 hover:cursor-pointer"
 			>
 				Create Account
 			</button>
 		</form>
 	);
 }
-
