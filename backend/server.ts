6be6b13cfd3276ab343a5a3237ede460b0c99712
Hairum Qureshi <hqureshi@udel.edu>
2025-10-20 18:22:55 -0400

    set up passport and express-session

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import chalk from "chalk";
+import session from "express-session";
+import passport from "passport";
 
 dotenv.config();
 
@@ -18,6 +20,16 @@ app.use(cors(corsOptions)); // <-- for CORS
 app.use(cookieParser()); // <-- allows you to read req.cookies
 app.use(express.json()); // <-- without this, req.body won't work (for JSON data being passed to the backend)
 app.use(express.urlencoded({ extended: true })); // <-- without this, you won't be able to read form data
+app.use(
+	"session",
+	session({
+		secret: process.env.EXPRESS_SESSION_SECRET!,
+		resave: false,
+		saveUninitialized: false
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT: string | number = process.env.PORT || 9000;
 
