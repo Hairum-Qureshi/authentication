c57501d34d43aea58ce4c156c20ee4502ec27d45
Hairum Qureshi <hqureshi@udel.edu>
2025-10-20 18:47:05 -0400

    added IUser interface

---
+++ backend/interfaces.ts
@@ -0,0 +1,11 @@
+interface IUser {
+	firstName: string;
+	lastName: string;
+	email: string;
+	profilePicture: string;
+	password: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export type { IUser };
