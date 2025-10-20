9c990206156d1bba1628b16a979aeaf08f2216bf
Hairum Qureshi <hqureshi@udel.edu>
2025-10-19 20:17:22 -0400

    finalized sign in component form styling

--- frontend/src/components/SignIn.tsx
+++ frontend/src/components/SignIn.tsx
@@ -1,3 +1,23 @@
 export default function SignIn() {
-	return <div>SignIn</div>;
+	return (
+		<form className="flex flex-col space-y-7 my-8">
+			<input
+				type="email"
+				placeholder="Email"
+				className="rounded-md p-3 bg-slate-800 outline-none"
+			/>
+			<input
+				type="password"
+				placeholder="Password"
+				className="rounded-md p-3 bg-slate-800 outline-none"
+			/>
+
+			<button
+				type="submit"
+				className="flex w-full p-2 justify-center rounded-md bg-sky-600"
+			>
+				Sign In
+			</button>
+		</form>
+	);
 }
