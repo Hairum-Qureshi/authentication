ae1b9b8978f7d7283d39c4ba1783e258988062bf
Hairum Qureshi <hqureshi@udel.edu>
2025-10-20 01:05:43 -0400

    sorted out spacing issue between 'or' divider and button

--- frontend/src/components/SignIn.tsx
+++ frontend/src/components/SignIn.tsx
@@ -1,17 +1,18 @@
 export default function SignIn() {
 	return (
-		<form className="flex flex-col space-y-7 my-8">
-			<input
-				type="email"
-				placeholder="Email"
-				className="rounded-md p-3 bg-slate-800 outline-none focus:outline-none focus:border-sky-500 focus:border"
-			/>
-			<input
-				type="password"
-				placeholder="Password"
-				className="rounded-md p-3 bg-slate-800 outline-none focus:outline-none focus:border-sky-500 focus:border"
-			/>
-
+		<form className="my-8">
+			<div className="flex flex-col space-y-7 ">
+				<input
+					type="email"
+					placeholder="Email"
+					className="rounded-md p-3 bg-slate-800 outline-none focus:outline-none focus:border-sky-500 focus:border"
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					className="rounded-md p-3 bg-slate-800 outline-none focus:outline-none focus:border-sky-500 focus:border"
+				/>
+			</div>
 			<button
 				type="submit"
 				className="flex w-full p-2 justify-center rounded-md bg-sky-600 hover:cursor-pointer"
