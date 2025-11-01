fcacd412c2c06fe2cbc1a5cad04d56e09c94017c
Hairum Qureshi <hqureshi@udel.edu>
2025-10-31 21:39:50 -0400

    connected MongoDB database and added cookie configuration to Express Session middleware

--- backend/server.ts
+++ backend/server.ts
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser";
 import chalk from "chalk";
 import session from "express-session";
 import passport from "passport";
+import authentication from "./routes/authentication";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -25,16 +27,33 @@ app.use(
 	session({
 		secret: process.env.EXPRESS_SESSION_SECRET!,
 		resave: false,
-		saveUninitialized: false
+		saveUninitialized: false,
+		cookie: {
+			maxAge: 7 * 24 * 60 * 60 * 1000
+		}
 	})
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-const PORT: string | number = process.env.PORT || 9000;
+app.use("/api", authentication);
+
+const MONGO_URI: string = process.env.MONGO_URI!;
+const PORT: string | number = process.env.PORT! || 9000;
 
 app.listen(PORT, () => {
-	console.log(
-		chalk.bold(chalk.yellowBright(`Server listening on port ${PORT}!`))
-	);
+	const connectToMongoDB = async () => {
+		try {
+			const conn = await mongoose.connect(MONGO_URI!);
+			console.log(
+				chalk.yellowBright("Successfully connected to MongoDB on host:"),
+				chalk.greenBright(`${conn.connection.host}`)
+			);
+			console.log(chalk.yellowBright(`Server listening on port ${PORT}!`));
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	connectToMongoDB();
 });
