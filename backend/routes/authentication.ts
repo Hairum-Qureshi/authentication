6fa30d3b7b47691b577a5705c61762d1fa72f63a
Hairum Qureshi <hqureshi@udel.edu>
2025-10-31 22:19:50 -0400

    added routes for handling 2FA and getting current user

---
+++ backend/routes/authentication.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+	getCurrentUser,
+	getStatus,
+	reset2FA,
+	setup2FA,
+	signIn,
+	signOut,
+	signUp,
+	verify2FA
+} from "../controllers/authentication";
+
+const router = express.Router();
+
+router.post("/sign-up", signUp);
+router.post("/sign-in", signIn);
+router.post("/sign-out", signOut);
+router.get("/status", getStatus);
+router.get("/2fa/setup", setup2FA);
+router.post("/2fa/verify", verify2FA);
+router.post("/2fa/reset", reset2FA);
+router.get("/current-user", getCurrentUser);
+
+export default router;
