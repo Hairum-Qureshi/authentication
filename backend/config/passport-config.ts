6d9fd9b03bf15f82e3af960e3a99fde9cddbddad
Hairum Qureshi <hqureshi@udel.edu>
2025-11-01 01:24:18 -0400

    fixed passport auth middleware so it works as intended

---
+++ backend/config/passport-config.ts
@@ -0,0 +1,51 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcrypt";
+import User from "../models/User";
+import { IUser } from "../interfaces";
+import chalk from "chalk";
+
+passport.use(
+	new LocalStrategy(
+		{ usernameField: "email" },
+		async (email, password, done) => {
+			try {
+				if (!email || !password) {
+					return done(null, false, {
+						message: "Please enter both email and password."
+					});
+				}
+
+				const user: IUser | null = (await User.findOne({
+					email
+				})) as IUser | null;
+
+				if (!user) return done(null, false, { message: "User not found" });
+
+				const isMatch = await bcrypt.compare(password, user.password as string);
+				if (!isMatch)
+					return done(null, false, { message: "Incorrect password" });
+
+				passport.serializeUser((user, done) => {
+					done(null, (user as IUser)._id);
+				});
+
+				passport.deserializeUser(async (uid, done) => {
+					try {
+						const user = await User.findById(uid);
+						done(null, user);
+					} catch (err) {
+						done(err);
+					}
+				});
+
+				return done(null, user);
+			} catch (error) {
+				return done(
+					chalk.redBright("Error in Passport Local Strategy:", error),
+					false
+				);
+			}
+		}
+	)
+);
