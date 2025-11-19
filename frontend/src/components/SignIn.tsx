cff4c739fe6f6d7a18de23159a7571bbd01638d9
Hairum Qureshi <hqureshi@udel.edu>
2025-11-19 00:40:37 -0500

    added state to inputs and signUp function call from custom useAuth hook

--- frontend/src/components/SignIn.tsx
+++ frontend/src/components/SignIn.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import useAuth from "../hooks/useAuth";
+
 export default function SignIn() {
+	const { signIn } = useAuth();
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
 	return (
 		<form className="my-8">
 			<div className="flex flex-col space-y-7 ">
@@ -6,16 +14,24 @@ export default function SignIn() {
 					type="email"
 					placeholder="Email"
 					className="rounded-md p-3 bg-slate-800 outline-none focus:outline-none focus:border-sky-500 focus:border"
+					value={email}
+					onChange={e => setEmail(e.target.value)}
 				/>
 				<input
 					type="password"
 					placeholder="Password"
 					className="rounded-md p-3 bg-slate-800 outline-none focus:outline-none focus:border-sky-500 focus:border"
+					value={password}
+					onChange={e => setPassword(e.target.value)}
 				/>
 			</div>
 			<button
 				type="submit"
 				className="flex w-full p-2 justify-center rounded-md bg-sky-600 hover:cursor-pointer"
+				onClick={e => {
+					e.preventDefault();
+					signIn(email, password);
+				}}
 			>
 				Sign In
 			</button>
