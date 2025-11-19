eabd773caec9aa2662e9661dfceee0c7b39f916b
Hairum Qureshi <hqureshi@udel.edu>
2025-11-18 23:27:09 -0500

    replaced 'loading...' text with custom Loading component

--- frontend/src/components/pages/Profile.tsx
+++ frontend/src/components/pages/Profile.tsx
@@ -1,3 +1,11 @@
+import { useCurrentUser } from "../../hooks/useCurrentUser";
+import Loading from "../Loading";
+
 export default function Profile() {
-	return <div>Profile</div>;
+	const { data: currUserData } = useCurrentUser();
+
+	if (!currUserData) {
+		return <Loading />;
+	}
+	return <div>{JSON.stringify(currUserData, null, 2)}</div>;
 }
