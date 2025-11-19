6f476a18e37fdf9041d531630351f9d01dc8c9e2
Hairum Qureshi <hqureshi@udel.edu>
2025-11-19 00:49:15 -0500

    changed 'profilePicture' to snake case

--- backend/interfaces.ts
+++ backend/interfaces.ts
@@ -5,7 +5,7 @@ type User = {
 	firstName: string;
 	lastName: string;
 	email: string;
-	profilePicture: string;
+	profile_picture: string;
 	password?: string | undefined;
 	isMFAEnabled: boolean;
 	isGoogleAccount: boolean;
