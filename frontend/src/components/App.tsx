9cfe63ccb5b89798b3ac0ca156fca48345de15ad
Hairum Qureshi <hqureshi@udel.edu>
2025-11-18 23:26:21 -0500

    wrapped Profile route component in a custom ProtectedRoutesGuard component

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -2,13 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import ProtectedRoutesGuard from "./ProtectedRoutesGuard";
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Landing />} />
-				<Route path="/profile" element={<Profile />} />
+				<Route
+					path="/profile"
+					element={
+						<ProtectedRoutesGuard>
+							<Profile />
+						</ProtectedRoutesGuard>
+					}
+				/>
+				;
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
